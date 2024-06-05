071e9bebaad9d9056d92e8dc9a8014246d210f5b
Hedi Fourati <127599179+dido2007@users.noreply.github.com>
2024-06-05 11:36:48 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import SigninButton from "@/components/auth/SigninButton";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <p> login </p>
         <SigninButton />
     </main>
   );
